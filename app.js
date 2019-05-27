e5132f12dfadb2ca6d8ae615841e35bab53c9c72
hitesh0304 <200378502@student.georgianc.on.ca>
2019-05-27 17:15:56 -0400

    new commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express= require(`express`);
+
+const app = express();
+app.get(`/`, (request, response) =>{
+    response.send(`Welcome to our very plain site.`);
+});
+
+app.get(`/about`,(request, response) => {
+    response.send(`It is a cold dark rainy day in Pizzaville.`);
+});
+
+app.listen(4000, () => console.log(`listening on port 4000`));
